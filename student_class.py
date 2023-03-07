299ef445b4acf27fa04e72333ca4be957895552a
Muhammad Arif <123452078+F22BSEEN1E02102@users.noreply.github.com>
2023-03-07 11:41:51 +0500

    Add files via upload

---
+++ student_class.py
@@ -0,0 +1,19 @@
+class Car:
+    def __init__(self,speed,color):
+        self.speed="speed"
+        self.color="color"
+
+        print("This is my car its name is ")
+        print(color)
+        print(speed)
+
+Honda=Car(200,"red")
+Honda=Car(250,"Green")
+Audi=Car(300,"blue")
+
+print(Honda.color)
+
+
+
+
+
